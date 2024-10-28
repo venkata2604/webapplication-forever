6211070ac0f4330fef404f371b66f64a592f0b3f
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2024-10-28 13:26:36 -0500

    updates

---
+++ frontened/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingIndicator.css"
+
+const LoadingIndicator = () => {
+    return <div className="Loading-container">
+        <div className="Loader"></div>
+    </div>
+}
+
+export default LoadingIndicator
